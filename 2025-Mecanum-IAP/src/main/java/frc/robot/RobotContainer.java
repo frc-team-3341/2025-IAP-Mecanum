c95a933002d5560091df4ea44145b86b5c06a941
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2025-11-08 14:05:49 -0800

    Addition of gradle files

--- 2025-Mecanum-IAP/src/main/java/frc/robot/RobotContainer.java
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/RobotContainer.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj.templates.commandbased;
+package frc.robot;
 
-import edu.wpi.first.wpilibj.templates.commandbased.Constants.OperatorConstants;
-import edu.wpi.first.wpilibj.templates.commandbased.commands.Autos;
-import edu.wpi.first.wpilibj.templates.commandbased.commands.ExampleCommand;
-import edu.wpi.first.wpilibj.templates.commandbased.subsystems.ExampleSubsystem;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
--- 2025-Mecanum-IAP/src/main/java/frc/robot/commands/ExampleCommand.java
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj.templates.commandbased.commands;
+package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.templates.commandbased.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
