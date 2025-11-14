d8cb331fea1dbcf9b9b21f2216637db1a0f44959
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2025-11-13 17:16:58 -0800

    Creation of MecanumDrive, and DriveTrain Classes

---
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/commands/MecanumDrive.java
@@ -0,0 +1,60 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.DriveTrain;
+
+public class MecanumDrive extends Command {
+
+    private final DriveTrain driveTrain;
+    private final XboxController controller;
+
+    public MecanumDrive(DriveTrain driveTrain, XboxController controller) {
+        this.driveTrain = driveTrain;
+        this.controller = controller;
+        addRequirements(driveTrain);
+    }
+
+    private double deadband(double val) {
+        return Math.abs(val) < 0.05 ? 0 : val;
+    }
+
+    @Override
+    public void execute() {
+        double forward = deadband(-controller.getLeftY());
+        double strafe  = deadband(controller.getLeftX());
+        double rotation = deadband(controller.getRightX()) * 0.5;
+
+        // Telemetry: joystick values
+        SmartDashboard.putNumber("Joystick Forward", forward);
+        SmartDashboard.putNumber("Joystick Strafe", strafe);
+        SmartDashboard.putNumber("Joystick Rotation", rotation);
+
+        double robotAngleRad = Math.toRadians(driveTrain.getAngle());
+
+        ChassisSpeeds fieldOrientedSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                forward,
+                strafe,
+                rotation,
+                new Rotation2d(robotAngleRad)
+        );
+
+        driveTrain.drive(fieldOrientedSpeeds);
+
+        // Telemetry: robot heading
+        SmartDashboard.putNumber("Robot Heading (deg)", driveTrain.getAngle());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        driveTrain.drive(0, 0, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,77 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Studica NavX imports
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+public class DriveTrain extends SubsystemBase {
+
+    private final PWMSparkMax frontLeftMotor  = new PWMSparkMax(0);
+    private final PWMSparkMax backLeftMotor   = new PWMSparkMax(1);
+    private final PWMSparkMax frontRightMotor = new PWMSparkMax(2);
+    private final PWMSparkMax backRightMotor  = new PWMSparkMax(3);
+
+    private final AHRS navx = new AHRS(NavXComType.kMXP_SPI);
+
+    private static final double ROBOT_WIDTH  = 0.6;
+    private static final double ROBOT_LENGTH = 0.6;
+    private static final double MAX_WHEEL_SPEED = 1.0;
+
+    private final MecanumDriveKinematics kinematics = new MecanumDriveKinematics(
+            new Translation2d(ROBOT_LENGTH / 2,  ROBOT_WIDTH / 2),   // Front Left
+            new Translation2d(ROBOT_LENGTH / 2, -ROBOT_WIDTH / 2),   // Front Right
+            new Translation2d(-ROBOT_LENGTH / 2, ROBOT_WIDTH / 2),   // Back Left
+            new Translation2d(-ROBOT_LENGTH / 2, -ROBOT_WIDTH / 2)   // Back Right
+    );
+
+    public DriveTrain() {
+        frontLeftMotor.setInverted(true);
+        backLeftMotor.setInverted(true);
+    }
+
+    public void drive(ChassisSpeeds speeds) {
+        MecanumDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(speeds);
+        wheelSpeeds.desaturate(MAX_WHEEL_SPEED);
+
+        frontLeftMotor.set(wheelSpeeds.frontLeftMetersPerSecond / MAX_WHEEL_SPEED);
+        frontRightMotor.set(wheelSpeeds.frontRightMetersPerSecond / MAX_WHEEL_SPEED);
+        backLeftMotor.set(wheelSpeeds.rearLeftMetersPerSecond / MAX_WHEEL_SPEED);
+        backRightMotor.set(wheelSpeeds.rearRightMetersPerSecond / MAX_WHEEL_SPEED);
+
+        // Telemetry: wheel speeds
+        SmartDashboard.putNumber("FL Wheel Speed", wheelSpeeds.frontLeftMetersPerSecond);
+        SmartDashboard.putNumber("FR Wheel Speed", wheelSpeeds.frontRightMetersPerSecond);
+        SmartDashboard.putNumber("BL Wheel Speed", wheelSpeeds.rearLeftMetersPerSecond);
+        SmartDashboard.putNumber("BR Wheel Speed", wheelSpeeds.rearRightMetersPerSecond);
+    }
+
+    public void drive(double forward, double strafe, double rotation) {
+        drive(new ChassisSpeeds(forward, strafe, rotation));
+    }
+
+    public double getAngle() {
+        return navx.getAngle();
+    }
+
+    public void resetNavx() {
+        navx.reset();
+    }
+
+    @Override
+    public void periodic() {
+        // Telemetry
+        SmartDashboard.putNumber("NavX Angle", getAngle());
+        SmartDashboard.putNumber("Front Left Motor Output", frontLeftMotor.get());
+        SmartDashboard.putNumber("Front Right Motor Output", frontRightMotor.get());
+        SmartDashboard.putNumber("Back Left Motor Output", backLeftMotor.get());
+        SmartDashboard.putNumber("Back Right Motor Output", backRightMotor.get());
+    }
+}
