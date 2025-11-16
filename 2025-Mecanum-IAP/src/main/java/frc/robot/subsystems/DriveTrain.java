b307ace188b978f0a0303171f65b89c53e2bdddd
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2025-11-15 18:51:16 -0800

    Update DriveTrain with the Correct SparkMax Libraries

--- 2025-Mecanum-IAP/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -3,11 +3,16 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
 
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //Studica Imports
@@ -16,17 +21,18 @@ import com.studica.frc.AHRS.NavXComType;
 
 public class DriveTrain extends SubsystemBase {
 
-    private final PWMSparkMax frontLeftMotor  = new PWMSparkMax(0);
-    private final PWMSparkMax backLeftMotor   = new PWMSparkMax(1);
-    private final PWMSparkMax frontRightMotor = new PWMSparkMax(2);
-    private final PWMSparkMax backRightMotor  = new PWMSparkMax(3);
+    private final SparkMax frontLeftMotor  = new SparkMax(1,SparkMax.MotorType.kBrushless);
+    private final SparkMax backLeftMotor   = new SparkMax(2,SparkMax.MotorType.kBrushless);
+    private final SparkMax frontRightMotor = new SparkMax(3,SparkMax.MotorType.kBrushless);
+    private final SparkMax backRightMotor  = new SparkMax(4,SparkMax.MotorType.kBrushless);
 
     private final AHRS navx = new AHRS(NavXComType.kMXP_SPI);
-
-    private static final double ROBOT_WIDTH  = 0.6;
-    private static final double ROBOT_LENGTH = 0.6;
-    private static final double MAX_WHEEL_SPEED = 1.0;
-
+    //Temporary Values for the dimensions
+    private static final double ROBOT_WIDTH  = 0.32;
+    private static final double ROBOT_LENGTH = 0.43;
+    private static final double MAX_WHEEL_SPEED = 4.0;
+    public static final double MAX_ANGULAR_SPEED_RAD_PER_SEC = Math.PI; 
+    //Left is + and forward is also +
     private final MecanumDriveKinematics kinematics = new MecanumDriveKinematics(
             new Translation2d(ROBOT_LENGTH / 2,  ROBOT_WIDTH / 2),   // Front Left
             new Translation2d(ROBOT_LENGTH / 2, -ROBOT_WIDTH / 2),   // Front Right
@@ -35,31 +41,42 @@ public class DriveTrain extends SubsystemBase {
     );
 
     public DriveTrain() {
-        frontLeftMotor.setInverted(true);
-        backLeftMotor.setInverted(true);
+        // Configure motors with correct inversion
+        configureMotor(frontLeftMotor, true);
+        configureMotor(backLeftMotor,  true);
+        configureMotor(frontRightMotor,false);
+        configureMotor(backRightMotor, false);
+    }
+
+    private void configureMotor(SparkMax motor, boolean inverted) {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.inverted(inverted);
+        motor.configure(config, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+
     }
 
     public void drive(ChassisSpeeds speeds) {
         MecanumDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(speeds);
         wheelSpeeds.desaturate(MAX_WHEEL_SPEED);
-
         frontLeftMotor.set(wheelSpeeds.frontLeftMetersPerSecond / MAX_WHEEL_SPEED);
         frontRightMotor.set(wheelSpeeds.frontRightMetersPerSecond / MAX_WHEEL_SPEED);
         backLeftMotor.set(wheelSpeeds.rearLeftMetersPerSecond / MAX_WHEEL_SPEED);
         backRightMotor.set(wheelSpeeds.rearRightMetersPerSecond / MAX_WHEEL_SPEED);
         //Displaying wheelspeed on shufflboard
-        SmartDashboard.putNumber("FL Wheel Speed", wheelSpeeds.frontLeftMetersPerSecond);
-        SmartDashboard.putNumber("FR Wheel Speed", wheelSpeeds.frontRightMetersPerSecond);
-        SmartDashboard.putNumber("BL Wheel Speed", wheelSpeeds.rearLeftMetersPerSecond);
-        SmartDashboard.putNumber("BR Wheel Speed", wheelSpeeds.rearRightMetersPerSecond);
+        SmartDashboard.putNumber("FL Wheel Speed(m/s)", wheelSpeeds.frontLeftMetersPerSecond);
+        SmartDashboard.putNumber("FR Wheel Speed(m/s)", wheelSpeeds.frontRightMetersPerSecond);
+        SmartDashboard.putNumber("BL Wheel Speed(m/s)", wheelSpeeds.rearLeftMetersPerSecond);
+        SmartDashboard.putNumber("BR Wheel Speed(m/s)", wheelSpeeds.rearRightMetersPerSecond);
     }
 
-    public void drive(double forward, double strafe, double rotation) {
-        drive(new ChassisSpeeds(forward, strafe, rotation));
+    public void drive(double forwardMps, double strafeMps, double rotRadPerSec) {
+        drive(new ChassisSpeeds(forwardMps, strafeMps, rotRadPerSec));
     }
 
-    public double getAngle() {
-        return navx.getAngle();
+    public Rotation2d getHeadingRotation2d() {
+        double wrapped = Math.IEEEremainder(navx.getAngle(), 360.0);
+        //neg for field centric
+        return Rotation2d.fromDegrees(-wrapped);
     }
 
     public void resetNavx() {
@@ -69,10 +86,6 @@ public class DriveTrain extends SubsystemBase {
     @Override
     public void periodic() {
         //Readings
-        SmartDashboard.putNumber("NavX Angle", getAngle());
-        SmartDashboard.putNumber("Front Left Motor Output", frontLeftMotor.get());
-        SmartDashboard.putNumber("Front Right Motor Output", frontRightMotor.get());
-        SmartDashboard.putNumber("Back Left Motor Output", backLeftMotor.get());
-        SmartDashboard.putNumber("Back Right Motor Output", backRightMotor.get());
+        SmartDashboard.putNumber("NavX Angle", navx.getAngle());
     }
 }
