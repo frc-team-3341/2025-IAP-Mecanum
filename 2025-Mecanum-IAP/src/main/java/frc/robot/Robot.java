c31cc247ed3720becd476b4a781327b9b8d73d34
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2025-12-01 17:42:13 -0800

    Added Simulation Files and Changes to Make Lateral Movement Accurate

--- 2025-Mecanum-IAP/src/main/java/frc/robot/Robot.java
+++ 2025-Mecanum-IAP/src/main/java/frc/robot/Robot.java
@@ -39,7 +39,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    CommandScheduler.getInstance().run();
+    
   }
   @Override
   public void robotPeriodic() {
